1c5fe5d3661eceed233de6f87f619b7c361f2633
niraj-s20 <32014853+niraj-s20@users.noreply.github.com>
2020-07-17 20:58:40 +0530

    third commit
    no sign in

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import './App.css';
-import Header from './Components/Header/Header';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import Create from './Pages/CreateBoard/CreateBoard';
-import Board from './Pages/Board/Board';
-import Login from './Pages/Login/Login';
-import SignUp from './Pages/Signup/Signup';
-import Authentication from './Context/Authentication';
-import PrivateRoute from './PrivateRoute';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./views/home/Home";
+import CreateBoard from "./views/addBoard/CreateBoard";
+import PageNotFound from "./views/pageNotFound/PageNotFound";
+import Board from "./views/board/Board";
 
 function App() {
   return (
-    <Authentication>
-      <Router>
-        <Header/>
-        <Switch>
-        <PrivateRoute exact path="/" component={Home} />
-        <PrivateRoute path="/createboard" component={Create}/>
-        <PrivateRoute path="/boarbgfb h hg f  bd/:boardid" component={Board}/>
-        <Route path="/signup" component={SignUp}/>
-        <Route path="/login" component={Login}/>
-        <Route exact path="*" component={Home}/>
-        </Switch>
-      </Router>
-    </Authentication>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/createboard" component={CreateBoard}></Route>
+        <Route exact path="/:boardName" component={Board}></Route>
+
+        <Route path="*" component={PageNotFound}></Route>
+      </Switch>
+    </Router>
   );
 }
 
